4b43be01422435c4bde48c3e1b87902b349346e1
Angel Chung <angeldeveloper1@gmail.com>
2024-03-27 14:10:48 -0500

    Agregacion de Exceptions y codigos de respuesta HTTP. Creacion de neuva carpeta llamada exception, con 2 clases nuevas dentro de ella.

---
+++ src/main/java/com/biblioteca/exception/ApiExceptionBody.java
@@ -0,0 +1,29 @@
+package com.biblioteca.exception;
+
+import java.time.ZonedDateTime;
+
+public class ApiExceptionBody {
+    private final String message;
+    private final ZonedDateTime zonedDateTime;
+
+    public ApiExceptionBody(String message, ZonedDateTime zonedDateTime) {
+        this.message = message;
+        this.zonedDateTime = zonedDateTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ZonedDateTime getZonedDateTime() {
+        return zonedDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiExceptionBody{" +
+                "message='" + message + '\'' +
+                ", zonedDateTime=" + zonedDateTime +
+                '}';
+    }
+}
